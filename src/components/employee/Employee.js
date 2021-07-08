0b845c5247e292c7745eb4715bb8429c87a65f69
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-07-08 15:09:42 -0500

    created data provider, list, and individual card item components for locations, employees, and customers that correspond to the animals resource to display all data loaded from the kennel-api database; refactored ApplicationViews component to render each list component

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -1,9 +1,16 @@
 import React from "react"
 import "./Employee.css"
 
-export const EmployeeCard = ({employeeName, employeeLocation}) => (
+// export const EmployeeCard = ({employeeName, employeeLocation}) => (
+//     <section className="employee">
+//         <h3 className="employee__name">{employeeName}</h3>
+//         <div className="employee__location">{employeeLocation}</div>
+//     </section>
+// )
+
+export const EmployeeCard = ({ employee }) => (
     <section className="employee">
-        <h3 className="employee__name">{employeeName}</h3>
-        <div className="employee__location">{employeeLocation}</div>
+        <h3 className="employee__name">{employee.name}</h3>
+        <address className="employee__location">Location: {employee.location.name}</address>
     </section>
 )
